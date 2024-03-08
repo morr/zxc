52afcd011d60205829615980993d8b439690975e
Andrey Sidorov <takandar@gmail.com>
2024-03-08 20:48:13 +0300

    basic app

---
+++ src/configs.rs
@@ -0,0 +1,4 @@
+// pub const BG_COLOR: (u8, u8, u8) = (181, 212, 220);
+pub const BG_COLOR: (u8, u8, u8) = (180, 180, 180);
+pub const WW: usize = 1920;
+pub const WH: usize = 1080;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-use bevy::prelude::*;
+pub mod configs;
+pub use configs::*;
+
+// use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::close_on_esc,
+};
+use bevy_pancam::{PanCam, PanCamPlugin};
 
 fn main() {
-    // println!("Hello, world!");
-    // rand::
-    // let mut guess = "zxc";
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        mode: bevy::window::WindowMode::Windowed,
+                        resolution: (WW as f32, WH as f32).into(),
+                        title: "Zxc".to_string(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_plugins(PanCamPlugin::default())
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .insert_resource(ClearColor(Color::rgba_u8(
+            BG_COLOR.0, BG_COLOR.1, BG_COLOR.2, 0,
+        )))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands
+        .spawn(Camera2dBundle::default())
+        .insert(PanCam::default());
 }
