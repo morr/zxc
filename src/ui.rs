7fa9c9d8f0cd9de1549d3c9bb794bb35c2b9e8e1
Andrey Sidorov <takandar@gmail.com>
2024-03-18 00:20:05 +0300

    additional text

--- src/ui.rs
+++ src/ui.rs
@@ -23,18 +23,43 @@ fn render_ui(
     time_state: Res<State<TimeState>>,
     asset_server: Res<AssetServer>,
 ) {
+    let font = asset_server.load("fonts/FiraMono-Medium.ttf");
     commands.spawn((
         TextBundle::from_section(
             format_ui_line(&settings, &time_state),
             TextStyle {
-                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font: font.clone(),
                 font_size: 24.,
                 color: Color::WHITE,
             },
         ),
         DebugLine {},
     ));
+    commands.spawn(
+        TextBundle::from_section(
+            "\"space\" - pause\n\"=\"/\"-\" - change game speed\n\"esc\" - hide help",
+            TextStyle {
+                font: font.clone(),
+                font_size: 16.,
+                color: Color::WHITE,
+            },
+        )
+        .with_style(Style {
+            // position_type: PositionType::Absolute,
+            left: Val::Px(0.0),
+            top: Val::Px(25.0),
+            ..default()
+        }),
+    );
 }
+
+// ,
+// position: UiRect {
+//     bottom: Val::Px(5.0),
+//     right: Val::Px(15.0),
+//     ..default()
+// },
+// ..default()
 
 fn update_ui(
     settings: Res<Settings>,
