0cf78fac976d14e4150b42555a0aa566a1848c90
Andrey Sidorov <takandar@gmail.com>
2024-03-19 23:05:09 +0300

    render base as sprite

--- src/structure/components.rs
+++ src/structure/components.rs
@@ -1,7 +1,7 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
-pub const BASE_WIDTH: f32 = 8.0;
-pub const BASE_HEIGHT: f32 = 12.0;
+pub const BASE_WIDTH: f32 = 7.0;
+pub const BASE_HEIGHT: f32 = 13.0;
 
 #[derive(Component)]
 pub struct Structure {}
@@ -10,5 +10,6 @@ pub struct Structure {}
 pub struct StructureBundle {
     pub structure: Structure,
     pub name: Name,
-    pub mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
+    // pub mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
+    pub sprite_bundle: SpriteBundle,
 }
--- src/structure/systems.rs
+++ src/structure/systems.rs
@@ -1,30 +1,36 @@
 use crate::{utils::tile_pos_to_world, GRID_COLS, GRID_ROWS, STRUCTURE_Z_INDEX, TILE_SIZE};
 
 use super::components::*;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
 
 pub fn spawn_base(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     println!("Spawning base");
 
-    let mesh = Mesh::from(Rectangle::new(
-        BASE_WIDTH * TILE_SIZE,
-        BASE_HEIGHT * TILE_SIZE,
-    ));
-    let material = ColorMaterial::from(Color::rgb(1., 0., 0.));
+    // let mesh = Mesh::from(Rectangle::new(
+    //     BASE_WIDTH * TILE_SIZE,
+    //     BASE_HEIGHT * TILE_SIZE,
+    // ));
+    // let material = ColorMaterial::from(Color::rgb(1., 0., 0.));
 
-    let mesh_handle = meshes.add(mesh);
-    let material_handle = materials.add(material);
+    // let mesh_handle = meshes.add(mesh);
+    // let material_handle = materials.add(material);
+    // https://fin-nio.itch.io/pixel-houses
+    let texture_handle = asset_server.load("sprites/castle_complete.png");
 
     commands.spawn((StructureBundle {
         structure: Structure {},
         name: Name::new("Base"),
-        mesh_bundle: MaterialMesh2dBundle {
-            mesh: mesh_handle.into(),
-            material: material_handle,
+        sprite_bundle: SpriteBundle {
+            texture: texture_handle.clone(),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(BASE_WIDTH * TILE_SIZE, BASE_HEIGHT * TILE_SIZE)),
+                ..default()
+            },
             transform: Transform::from_xyz(
                 tile_pos_to_world(GRID_COLS as f32 / 2.0),
                 tile_pos_to_world(GRID_ROWS as f32 / 2.0),
@@ -32,5 +38,15 @@ pub fn spawn_base(
             ),
             ..default()
         },
+        // mesh_bundle: MaterialMesh2dBundle {
+        //     mesh: mesh_handle.into(),
+        //     material: material_handle,
+        //     transform: Transform::from_xyz(
+        //         tile_pos_to_world(GRID_COLS as f32 / 2.0),
+        //         tile_pos_to_world(GRID_ROWS as f32 / 2.0),
+        //         STRUCTURE_Z_INDEX,
+        //     ),
+        //     ..default()
+        // },
     },));
 }
