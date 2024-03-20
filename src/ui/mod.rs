fe9e02de26bdc64c7553fb8521a29df981cc0e43
Andrey Sidorov <takandar@gmail.com>
2024-03-20 22:46:56 +0300

    toggle ui node visibility intead of spawning/despawning it

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -43,69 +43,6 @@ fn render_ui(
         ),
         TimeText {},
     ));
-    spawn_help(&mut commands, &asset_server);
-}
-
-fn update_ui(
-    elapsed_time: Res<ElapsedTime>,
-    time_state: Res<State<TimeState>>,
-    time_scale: Res<TimeScale>,
-    // mut ev_update_ui: EventReader<UpdateUiEvent>,
-    mut debug_query: Query<&mut Text, With<TimeText>>,
-) {
-    // for _ev in ev_update_ui.read() {
-    //     println!("update ui");
-
-    let mut text = debug_query.single_mut();
-    text.sections[0].value = format_ui_line(&elapsed_time, &time_state, &time_scale);
-    // }
-}
-
-fn format_ui_line(
-    elapsed_time: &Res<ElapsedTime>,
-    time_state: &Res<State<TimeState>>,
-    time_scale: &Res<TimeScale>,
-) -> String {
-    let speed_part = match time_state.get() {
-        TimeState::Running => format!("Speed: {}x", time_scale.0),
-        TimeState::Paused => "Paused".to_string(),
-    };
-
-    format!("Seconds: {} {}", elapsed_time.0.floor(), speed_part)
-}
-
-fn handle_ui_keys(
-    mut commands: Commands,
-    keys: Res<ButtonInput<KeyCode>>,
-    // query: Query<Entity>,
-    query: Query<Entity, With<HelpText>>,
-    asset_server: Res<AssetServer>,
-    debug_grid_state: Res<State<DebugGridState>>,
-    mut next_debug_grid_state: ResMut<NextState<DebugGridState>>,
-) {
-    if keys.just_pressed(KeyCode::KeyH) {
-        match query.get_single() {
-            Ok(entity) => {
-                commands.entity(entity).despawn();
-            }
-            Err(QuerySingleError::NoEntities(_)) => {
-                spawn_help(&mut commands, &asset_server);
-            }
-            Err(QuerySingleError::MultipleEntities(_)) => {
-                panic!("Error: There is more than one help text!");
-            }
-        }
-    }
-
-    if keys.just_pressed(KeyCode::KeyG) {
-        match debug_grid_state.get() {
-            DebugGridState::Visible => next_debug_grid_state.set(DebugGridState::Hidden),
-            DebugGridState::Hidden => next_debug_grid_state.set(DebugGridState::Visible),
-        };
-    }
-}
-
-fn spawn_help(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     commands.spawn((
         TextBundle::from_section(
             "\"space\" - pause
@@ -126,4 +63,73 @@ fn spawn_help(commands: &mut Commands, asset_server: &Res<AssetServer>) {
         }),
         HelpText {},
     ));
+}
+
+fn update_ui(
+    elapsed_time: Res<ElapsedTime>,
+    time_state: Res<State<TimeState>>,
+    time_scale: Res<TimeScale>,
+    // mut ev_update_ui: EventReader<UpdateUiEvent>,
+    mut query: Query<&mut Text, With<TimeText>>,
+) {
+    // for _ev in ev_update_ui.read() {
+    //     println!("update ui");
+
+    let mut text = query.single_mut();
+    text.sections[0].value = format_ui_line(&elapsed_time, &time_state, &time_scale);
+    // }
+}
+
+fn format_ui_line(
+    elapsed_time: &Res<ElapsedTime>,
+    time_state: &Res<State<TimeState>>,
+    time_scale: &Res<TimeScale>,
+) -> String {
+    let speed_part = match time_state.get() {
+        TimeState::Running => format!("Speed: {}x", time_scale.0),
+        TimeState::Paused => "Paused".to_string(),
+    };
+
+    format!("Seconds: {} {}", elapsed_time.0.floor(), speed_part)
+}
+
+fn handle_ui_keys(
+    // mut commands: Commands,
+    keys: Res<ButtonInput<KeyCode>>,
+    // query: Query<Entity>,
+    mut query: Query<&mut Visibility, With<HelpText>>,
+    debug_grid_state: Res<State<DebugGridState>>,
+    mut next_debug_grid_state: ResMut<NextState<DebugGridState>>,
+) {
+    if keys.just_pressed(KeyCode::KeyH) {
+        // commands.entity(query.single_mut()).iis
+        let mut visibility = query.single_mut();
+
+        match *visibility {
+            Visibility::Visible => {
+                *visibility = Visibility::Hidden;
+            }
+            _ => {
+                *visibility = Visibility::Visible;
+            }
+        }
+        // match query.get_single() {
+        //     Ok(entity) => {
+        //         commands.entity(entity).despawn();
+        //     }
+        //     Err(QuerySingleError::NoEntities(_)) => {
+        //         spawn_help(&mut commands, &asset_server);
+        //     }
+        //     Err(QuerySingleError::MultipleEntities(_)) => {
+        //         panic!("Error: There is more than one help text!");
+        //     }
+        // }
+    }
+
+    if keys.just_pressed(KeyCode::KeyG) {
+        match debug_grid_state.get() {
+            DebugGridState::Visible => next_debug_grid_state.set(DebugGridState::Hidden),
+            DebugGridState::Hidden => next_debug_grid_state.set(DebugGridState::Visible),
+        };
+    }
 }
