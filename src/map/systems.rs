48593c0fb6d1812342966c8f75528cd6b49bc50c
Andrey Sidorov <takandar@gmail.com>
2024-03-24 02:19:04 +0300

    remove skipping hover logic

--- src/map/systems.rs
+++ src/map/systems.rs
@@ -1,24 +1,15 @@
 use bevy::prelude::*;
 
 use super::*;
-use crate::input::components::PrevHoveredTilePos;
 
 pub fn highlight_hovered_tile(
     mut commands: Commands,
     mut event_reader: EventReader<HoverTileEvent>,
     query_tiles_hovered: Query<Entity, With<TileHovered>>,
     query_tiles: Query<(&Tile, Entity)>,
-    current_hovered_tile_pos: Res<PrevHoveredTilePos>,
 ) {
     for event in event_reader.read() {
         remove_tile_hovered_from_other_tiles(&query_tiles_hovered, &mut commands);
-
-        if let Some(pos) = current_hovered_tile_pos.0 {
-            if pos != event.0 {
-                print!("skipping hover");
-                return;
-            }
-        }
 
         for (tile, entity) in query_tiles.iter() {
             if tile.0 == event.0 {
