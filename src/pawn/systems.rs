fa2c5274015eebac97afb940a30f281ae2470500
Andrey Sidorov <takandar@gmail.com>
2024-03-27 03:19:13 +0300

    smart movement systesm: it does not stuck, it does not overreaches, it switches to a next point if there is move movement distance available in this tick

--- src/pawn/systems.rs
+++ src/pawn/systems.rs
@@ -71,18 +71,33 @@ pub fn move_pawns(
     time_scale: Res<TimeScale>,
 ) {
     for (_entity, mut pawn, mut transform) in &mut query_pawns {
-        if pawn.move_path.is_empty() {
-            continue;
-        }
+        move_to_target_location(
+            &mut pawn,
+            &mut transform,
+            PAWN_SPEED * time.delta_seconds() * time_scale.0,
+        );
+    }
+}
 
-        let current_world = transform.translation.truncate();
-        let move_target_world = pawn.move_path.front().unwrap().grid_tile_center_to_world();
+pub fn move_to_target_location(pawn: &mut Pawn, transform: &mut Transform, distance_to_move: f32) {
+    let current_point_world: Vec2 = transform.translation.truncate();
 
-        let direction = (move_target_world - current_world).normalize_or_zero();
-        transform.translation +=
-            direction.extend(0.) * PAWN_SPEED * time.delta_seconds() * time_scale.0;
-        if (move_target_world - current_world).length() < 0.2 {
+    if let Some(target_point_tile) = pawn.move_path.front() {
+        let target_point_world = target_point_tile.grid_tile_center_to_world();
+        let direction = (target_point_world - current_point_world).normalize_or_zero();
+        let distance_between_points = (target_point_world - current_point_world).length();
+
+        if distance_to_move >= distance_between_points {
+            transform.translation = target_point_world.extend(transform.translation.z);
             pawn.move_path.pop_front();
+
+            let remaining_distance = distance_to_move - distance_between_points;
+
+            if remaining_distance > 0.0 {
+                move_to_target_location(pawn, transform, remaining_distance);
+            }
+        } else {
+            transform.translation += (direction * distance_to_move).extend(0.0);
         }
     }
 }
