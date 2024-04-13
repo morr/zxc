5d257f91983734deb92db0cc14ee8bc83630dd36
Andrey Sidorov <takandar@gmail.com>
2024-04-13 22:58:34 +0300

    updates

--- src/daylight.rs
+++ src/daylight.rs
@@ -1,12 +1,32 @@
-use bevy::sprite::Material2dPlugin;
-
 use crate::*;
 
 pub struct DaylightPlugin;
 
 impl Plugin for DaylightPlugin {
     fn build(&self, app: &mut App) {
-        // app
+        app.add_plugins(BevyMagicLight2DPlugin)
+            .register_type::<BevyMagicLight2DSettings>()
+            .register_type::<LightPassParams>()
+            .insert_resource(BevyMagicLight2DSettings {
+                light_pass_params: LightPassParams {
+                    reservoir_size: 8,
+                    smooth_kernel_size: (3, 3),
+                    direct_light_contrib: 0.5,
+                    indirect_light_contrib: 0.5,
+                    ..default()
+                },
+                ..default()
+            });
+        // .insert_resource(BevyMagicLight2DSettings {
+        //     // light_pass_params: LightPassParams {
+        //     //     reservoir_size: 32,
+        //     //     smooth_kernel_size: (3, 3),
+        //     //     direct_light_contrib: 0.5,
+        //     //     indirect_light_contrib: 0.5,
+        //     //     ..default()
+        //     // },
+        //     ..default()
+        // })
         //     .add_plugin(Material2dPlugin::<StandardMaterial2d>::default())
         //     .add_systems(OnExit(WorldState::Loading), setup_light);
     }
