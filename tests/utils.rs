4cab88b03a5328259e5dd2d5ded7290834ee6ed5
Andrey Sidorov <takandar@gmail.com>
2024-04-28 21:33:34 +0300

    rename tests

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,21 +1,21 @@
 use zxc::{grid_tile_center_to_world, grid_tile_edge_to_world, world_pos_to_grid, CONFIG};
 
 #[test]
-fn transform_tile_pos_edge_to_world_pos() {
+fn grid_tile_edge_to_world_tests() {
     assert_eq!(grid_tile_edge_to_world(0), 0.0);
     assert_eq!(grid_tile_edge_to_world(1), CONFIG.tile.size);
     assert_eq!(grid_tile_edge_to_world(3), CONFIG.tile.size * 3.0);
 }
 
 #[test]
-fn transform_tile_pos_center_to_world_pos() {
+fn grid_tile_center_to_world_tests() {
     assert_eq!(grid_tile_center_to_world(0), CONFIG.tile.size / 2.0);
     assert_eq!(grid_tile_center_to_world(1), CONFIG.tile.size + CONFIG.tile.size / 2.0);
     assert_eq!(grid_tile_center_to_world(3), CONFIG.tile.size * 3.0 + CONFIG.tile.size / 2.0);
 }
 
 #[test]
-fn transform_world_pos_to_tile_pos() {
+fn world_pos_to_grid_tests() {
     assert_eq!(world_pos_to_grid(0.0), 0);
     assert_eq!(world_pos_to_grid(1.0), 0);
     assert_eq!(world_pos_to_grid(CONFIG.tile.size - 1.0), 0);
