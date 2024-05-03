5b249fe8b570d1b3f0251b3eb8bb20687f93723e
Andrey Sidorov <takandar@gmail.com>
2024-05-04 02:33:32 +0300

    extract farm_tile/systems

---
+++ src/structure/farm_tile/systems.rs
@@ -0,0 +1,36 @@
+use super::*;
+
+pub fn progress_farm_tile_state(
+    mut query: Query<(&Transform, &mut FarmTile)>,
+    mut commands: Commands,
+    mut event_reader: EventReader<FarmTileProgressEvent>,
+    assets: Res<FarmAssets>,
+) {
+    for event in event_reader.read() {
+        let entity = event.0;
+        let (transform, mut farm_tile) = query.get_mut(entity).unwrap();
+
+        farm_tile.progress_state(entity, &mut commands, transform, &assets);
+    }
+}
+
+pub fn progress_farm_tile_timer(
+    time: Res<Time>,
+    time_scale: Res<TimeScale>,
+    mut query: Query<(Entity, &Transform, &mut FarmTile), With<farm_tile_state::Planted>>,
+    mut commands: Commands,
+    assets: Res<FarmAssets>,
+) {
+    for (entity, transform, mut farm_tile) in query.iter_mut() {
+        let timer = match &mut farm_tile.state {
+            FarmTileState::Planted(timer) => timer,
+            _ => panic!("FarmTile must be in a timer-assigned state"),
+        };
+        timer.tick(time_scale.scale_to_duration(time.delta_seconds()));
+
+        if timer.finished() {
+            farm_tile.progress_state(entity, &mut commands, transform, &assets);
+        }
+    }
+}
+
