1daa08878af6d2b9bb0b505f90ea4a3101792c2d
Andrey Sidorov <takandar@gmail.com>
2024-05-08 16:38:51 +0300

    playing with ui

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,11 @@ fn main() {
             //     },
             // }),
         )
+
+        // .add_plugins(EguiPlugin)
+        // .add_plugins(bevy_inspector_egui::DefaultInspectorConfigPlugin) // adds default options and `InspectorEguiImpl`s
+        // .add_systems(OnExit(AppState::Loading), inspector_ui)
+
         // .add_plugins(WorldInspectorPlugin::new())
         // .add_plugins(FilterQueryInspectorPlugin::<With<structure::Structure>>::default())
         .add_plugins(FilterQueryInspectorPlugin::<With<Pawn>>::default())
@@ -72,3 +77,27 @@ fn main() {
         .add_systems(FixedUpdate, bevy::window::close_on_esc)
         .run();
 }
+
+// fn inspector_ui(world: &mut World) {
+//     let Ok(egui_context) = world
+//         .query_filtered::<&mut EguiContext, With<bevy::window::PrimaryWindow>>()
+//         .get_single(world)
+//     else {
+//         return;
+//     };
+//     let mut egui_context = egui_context.clone();
+//
+//     egui::Window::new("UI").show(egui_context.get_mut(), |ui| {
+//         egui::ScrollArea::vertical().show(ui, |ui| {
+//             // equivalent to `WorldInspectorPlugin`
+//             bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
+//
+//             egui::CollapsingHeader::new("Materials").show(ui, |ui| {
+//                 bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(world, ui);
+//             });
+//
+//             ui.heading("Entities");
+//             bevy_inspector_egui::bevy_inspector::ui_for_world_entities(world, ui);
+//         });
+//     });
+// }
