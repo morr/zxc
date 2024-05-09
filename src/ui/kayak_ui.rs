1c43d54b0205c471e0f2a9b135818615c75e2e27
Andrey Sidorov <takandar@gmail.com>
2024-05-09 22:23:57 +0300

    Revert "make it compile again"
    This reverts commit c8b7abe91dd90c16cd8269562bb9a4cda855c88b.

--- src/ui/kayak_ui.rs
+++ src/ui/kayak_ui.rs
@@ -4,6 +4,6 @@ pub fn setup_kayak_ui(
     mut commands: Commands,
     query: Query<Entity, With<MainCamera>>,
 ) {
-    // let entity = query.get_single().unwrap();
-    // commands.entity(entity).insert(CameraUIKayak);
+    let entity = query.get_single().unwrap();
+    commands.entity(entity).insert(CameraUIKayak);
 }
