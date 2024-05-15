a307cd269a5aec229e700d2d41f8598e08c3da46
Andrey Sidorov <takandar@gmail.com>
2024-05-16 00:30:58 +0300

    basic flawed selectable logic

--- src/selectable/mod.rs
+++ src/selectable/mod.rs
@@ -1,9 +1,52 @@
 use crate::*;
 
+use self::structure::Farm;
+
+#[derive(Component, Default)]
+pub struct UserSelected;
+
 pub struct SelectablePlugin;
 
 impl Plugin for SelectablePlugin {
-    fn build(&self, _app: &mut App) {
-        // app;
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, select_on_click.run_if(in_state(AppState::Playing)));
+    }
+}
+
+fn select_on_click(
+    mut commands: Commands,
+    mut click_event_reader: EventReader<ClickEvent>,
+    arc_navmesh: ResMut<ArcNavmesh>,
+) {
+    for event in click_event_reader.read() {
+        let grid_tile = event.0;
+        let navmesh = arc_navmesh.read();
+
+        // for target_id in navmesh.get_occupation::<Tile>(grid_tile.x, grid_tile.y) {
+        // }
+
+        for target_id in navmesh.get_occupation::<Movable>(grid_tile.x, grid_tile.y) {
+            commands
+                .entity(*target_id)
+                .insert(UserSelected::default())
+                .insert(ShowAabbGizmo {
+                    color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+                });
+
+            // if let Ok((pawn, movable)) = pawn_query.get(*target_id) {
+            // }
+        }
+
+        for target_id in navmesh.get_occupation::<Farm>(grid_tile.x, grid_tile.y) {
+            commands
+                .entity(*target_id)
+                .insert(UserSelected::default())
+                .insert(ShowAabbGizmo {
+                    color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+                });
+
+            // if let Ok((farm, workable)) = farm_query.get(*target_id) {
+            // }
+        }
     }
 }
