604324ef95879cbf9fd3e8bc525cb0830a2842f7
Andrey Sidorov <takandar@gmail.com>
2024-05-17 23:47:30 +0300

    spawn bed

--- src/structure/systems.rs
+++ src/structure/systems.rs
@@ -6,6 +6,7 @@ pub const FARM_TILE_SIZE: i32 = 1;
 pub const HOUSE_WIDTH: i32 = 4;
 pub const HOUSE_HEIGHT: i32 = 4;
 pub const WELL_SIZE: i32 = 2;
+pub const BED_SIZE: i32 = 1;
 
 pub fn spawn_base(
     mut commands: Commands,
@@ -15,26 +16,25 @@ pub fn spawn_base(
     let size = IVec2::new(BASE_WIDTH, BASE_HEIGHT);
     let grid_tile = IVec2::new(0, 0);
 
-    commands
-        .spawn((
-            Warehouse {},
-            Name::new("Warehouse"),
-            SpriteBundle {
-                texture: assets.castle.clone(),
-                sprite: Sprite {
-                    custom_size: Some(size.grid_tile_edge_to_world()),
-                    ..default()
-                },
-                transform: Transform::from_translation(
-                    (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
-                        .extend(STRUCTURE_Z_INDEX),
-                ),
+    commands.spawn((
+        Warehouse::default(),
+        Name::new("Warehouse"),
+        SpriteBundle {
+            texture: assets.castle.clone(),
+            sprite: Sprite {
+                custom_size: Some(size.grid_tile_edge_to_world()),
                 ..default()
             },
-        ));
-        // .insert(ShowAabbGizmo {
-        //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
-        // });
+            transform: Transform::from_translation(
+                (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
+                    .extend(STRUCTURE_Z_INDEX),
+            ),
+            ..default()
+        },
+    ));
+    // .insert(ShowAabbGizmo {
+    //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+    // });
 
     arc_navmesh.write().update_cost(
         (grid_tile.x)..(grid_tile.x + size.x),
@@ -78,26 +78,25 @@ pub fn spawn_house(
     let size = IVec2::new(HOUSE_WIDTH, HOUSE_HEIGHT);
     let grid_tile = IVec2::new(-13, -8);
 
-    commands
-        .spawn((
-            House {},
-            Name::new("House"),
-            SpriteBundle {
-                texture: assets.house_3.clone(),
-                sprite: Sprite {
-                    custom_size: Some(size.grid_tile_edge_to_world()),
-                    ..default()
-                },
-                transform: Transform::from_translation(
-                    (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
-                        .extend(STRUCTURE_Z_INDEX),
-                ),
+    commands.spawn((
+        House::default(),
+        Name::new("House"),
+        SpriteBundle {
+            texture: assets.house_3.clone(),
+            sprite: Sprite {
+                custom_size: Some(size.grid_tile_edge_to_world()),
                 ..default()
             },
-        ));
-        // .insert(ShowAabbGizmo {
-        //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
-        // });
+            transform: Transform::from_translation(
+                (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
+                    .extend(STRUCTURE_Z_INDEX),
+            ),
+            ..default()
+        },
+    ));
+    // .insert(ShowAabbGizmo {
+    //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+    // });
 
     arc_navmesh.write().update_cost(
         (grid_tile.x)..(grid_tile.x + size.x),
@@ -114,30 +113,67 @@ pub fn spawn_well(
     let size = IVec2::new(WELL_SIZE, WELL_SIZE);
     let grid_tile = IVec2::new(-3, -6);
 
-    commands
-        .spawn((
-            Well {},
-            Name::new("well"),
-            SpriteBundle {
-                texture: assets.well.clone(),
-                sprite: Sprite {
-                    custom_size: Some(size.grid_tile_edge_to_world()),
-                    ..default()
-                },
-                transform: Transform::from_translation(
-                    (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
-                        .extend(STRUCTURE_Z_INDEX),
-                ),
+    commands.spawn((
+        Well::default(),
+        Name::new("well"),
+        SpriteBundle {
+            texture: assets.well.clone(),
+            sprite: Sprite {
+                custom_size: Some(size.grid_tile_edge_to_world()),
                 ..default()
             },
-        ));
-        // .insert(ShowAabbGizmo {
-        //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
-        // });
+            transform: Transform::from_translation(
+                (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
+                    .extend(STRUCTURE_Z_INDEX),
+            ),
+            ..default()
+        },
+    ));
+    // .insert(ShowAabbGizmo {
+    //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+    // });
 
     arc_navmesh.write().update_cost(
         (grid_tile.x)..(grid_tile.x + size.x),
         (grid_tile.y)..(grid_tile.y + size.y),
         None,
     )
+}
+
+pub fn spawn_bed(
+    mut commands: Commands,
+    assets: Res<TextureAssets>,
+    arc_navmesh: ResMut<ArcNavmesh>,
+) {
+    let size = IVec2::new(BED_SIZE, BED_SIZE);
+    let grid_tile = IVec2::new(-7, 3);
+
+    let id = commands.spawn((
+        Bed::default(),
+        Name::new("bed"),
+        SpriteBundle {
+            texture: assets.bed.clone(),
+            sprite: Sprite {
+                custom_size: Some(size.grid_tile_edge_to_world()),
+                ..default()
+            },
+            transform: Transform::from_translation(
+                (grid_tile.grid_tile_edge_to_world() + size.grid_tile_edge_to_world() / 2.0)
+                    .extend(STRUCTURE_Z_INDEX),
+            ),
+            ..default()
+        },
+    )).id();
+    // .insert(ShowAabbGizmo {
+    //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+    // });
+
+    let mut navmesh = arc_navmesh.write();
+    navmesh.update_cost(
+        (grid_tile.x)..(grid_tile.x + size.x),
+        (grid_tile.y)..(grid_tile.y + size.y),
+        Some((2.0 * COST_MULTIPLIER) as i32),
+    );
+    navmesh.add_occupation::<Bed>(id, grid_tile.x, grid_tile.y);
+
 }
