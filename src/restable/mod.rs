f3e108a46494cbecc8c3c43a0aaacfe748376003
Andrey Sidorov <takandar@gmail.com>
2024-05-18 02:17:43 +0300

    basic stamina dragin/regain logic

--- src/restable/mod.rs
+++ src/restable/mod.rs
@@ -1,30 +1,41 @@
 use crate::*;
 
-expose_submodules!(components);
-// expose_submodules!(components, systems);
-
 pub struct RestablePlugin;
 
 impl Plugin for RestablePlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<Restable>();
-            // .add_event::<WorkCompleteEvent>()
-            // .add_event::<WorkStartEvent>()
-            // .add_systems(
-            //     FixedUpdate,
-            //     progress_work.run_if(in_state(SimulationState::Running)),
-            // )
-            // .add_systems(
-            //     FixedUpdate,
-            //     (
-            //         assign_tasks_to_pawns,
-            //         check_pawn_ready_for_working,
-            //         start_pawn_working,
-            //         complete_pawn_working,
-            //     )
-            //         .chain()
-            //         .run_if(in_state(AppState::Playing)),
-            // );
+        app.register_type::<Restable>()
+            .add_systems(Update, progress_stamina.run_if(in_state(AppState::Playing)));
     }
 }
 
+#[derive(Component, Debug, InspectorOptions, Reflect)]
+#[reflect(InspectorOptions)]
+pub struct Restable {
+    pub stamina: f32,
+}
+
+impl Default for Restable {
+    fn default() -> Self {
+        Self { stamina: 100.0 }
+    }
+}
+
+fn progress_stamina(
+    time: Res<Time>,
+    time_scale: Res<TimeScale>,
+    mut query: Query<(&mut Restable, &Pawn)>,
+) {
+    let time_amount = time_scale.scale_to_seconds(time.delta_seconds());
+
+    for (mut restable, pawn) in query.iter_mut() {
+        let diff = match pawn.state {
+            PawnState::Idle => time_amount * 0.3,
+            PawnState::Moving => -time_amount * 0.1,
+            PawnState::Working(_) => -time_amount * 4.0,
+            PawnState::Dead | PawnState::WorkAssigned(_) => 0.0,
+        };
+
+        restable.stamina = (restable.stamina + diff).clamp(0.0, 100.0);
+    }
+}
