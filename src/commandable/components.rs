05eb4f70b97d686aa69fb6d5ebc8f4a26a71d0f6
Andrey Sidorov <takandar@gmail.com>
2024-05-18 22:18:15 +0300

    CommandableState

--- src/commandable/components.rs
+++ src/commandable/components.rs
@@ -9,9 +9,19 @@ pub enum Command {
     MoveTo(Entity, IVec2),
 }
 
-#[derive(Component, Debug, Default)]
+#[derive(Component, Debug)]
 pub struct Commandable {
     pub queue: VecDeque<Command>,
+    pub state: CommandableState,
+}
+
+impl Default for Commandable {
+    fn default() -> Self {
+        Self {
+            queue: VecDeque::default(),
+            state: CommandableState::Empty
+        }
+    }
 }
 
 impl Commandable {
@@ -19,3 +29,57 @@ impl Commandable {
         self.queue.push_back(command);
     }
 }
+
+macro_rules! commandable_states {
+    (
+        $($name:ident),*
+    ) => {
+        #[derive(Debug, Clone, PartialEq, Eq, Reflect)]
+        pub enum CommandableState {
+            $($name),*
+        }
+
+        pub mod commandable_state {
+            use bevy::prelude::*;
+
+            $(
+                #[derive(Component, Reflect)]
+                pub struct $name;
+            )*
+        }
+
+        impl Commandable {
+            pub fn change_state(
+                &mut self,
+                new_state: CommandableState,
+                entity: Entity,
+                commands: &mut Commands
+            ) -> CommandableState {
+                use std::mem;
+
+                // println!("CommandableState {:?}=>{:?}", self.state, new_state);
+
+                // Remove the old state component
+                match &self.state {
+                    $(CommandableState::$name => {
+                        commands.entity(entity).remove::<commandable_state::$name>();
+                    },)*
+                }
+
+                // Set the new state and put old state into prev_state
+                let prev_state = mem::replace(&mut self.state, new_state);
+
+                // Add the new component
+                match &self.state {
+                    $(CommandableState::$name => {
+                        commands.entity(entity).insert(commandable_state::$name);
+                    },)*
+                }
+
+                prev_state
+            }
+        }
+    };
+}
+
+commandable_states!(Empty, Pending, Executing);
