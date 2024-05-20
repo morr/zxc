c0b823110e8c8271d2e8bf3319aca1cc0b1e815d
Andrey Sidorov <takandar@gmail.com>
2024-05-20 22:14:46 +0300

    MovableMoving => MovableStateMovingMarker

--- src/movable/systems.rs
+++ src/movable/systems.rs
@@ -2,7 +2,7 @@ use super::*;
 
 pub fn move_moving_entities(
     mut commands: Commands,
-    mut query_movable: Query<(Entity, &mut Movable, &mut Transform), With<MovableMoving>>,
+    mut query_movable: Query<(Entity, &mut Movable, &mut Transform), With<MovableStateMovingMarker>>,
     time: Res<Time>,
     time_scale: Res<TimeScale>,
     arc_navmesh: Res<ArcNavmesh>,
