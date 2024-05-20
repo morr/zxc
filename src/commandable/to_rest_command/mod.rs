8defdac52dbbba8d1c9017945a0e9f614fc9210b
Andrey Sidorov <takandar@gmail.com>
2024-05-21 02:04:57 +0300

    ToRestCommand logic

--- src/commandable/to_rest_command/mod.rs
+++ src/commandable/to_rest_command/mod.rs
@@ -12,9 +12,41 @@ impl Plugin for ToRestCommandPlugin {
 #[derive(Event, Debug, Clone, Reflect)]
 pub struct ToRestCommand(pub Entity);
 
-fn execute_command(mut command_reader: EventReader<ToRestCommand>) {
-    for command in command_reader.read() {
-        println!("{:?}", command);
-        // it does something
+fn execute_command(
+    mut commands: Commands,
+    bed_query: Query<&Transform, With<Bed>>,
+    mut commandable_query: Query<&mut Commandable>,
+    mut command_reader: EventReader<ToRestCommand>,
+    mut commandable_event_writer: EventWriter<CommandExecutedEvent>,
+) {
+    for ToRestCommand(entity) in command_reader.read() {
+        match commandable_query.get_mut(*entity) {
+            Ok(mut commandable) => {
+                let sleep_command_type = CommandType::Sleep(SleepCommand(*entity));
+
+                if let Some(transform) = bed_query.iter().next() {
+                    // either go to bed and sleep there
+                    commandable.extend_queue(
+                        [
+                            CommandType::MoveTo(MoveToCommand(
+                                *entity,
+                                transform.translation.truncate().world_pos_to_grid(),
+                            )),
+                            sleep_command_type,
+                        ],
+                        *entity,
+                        &mut commands,
+                    );
+                } else {
+                    // or sleep at the current spot
+                    commandable.extend_queue(sleep_command_type, *entity, &mut commands);
+                }
+                commandable.complete_command(*entity, &mut commands, &mut commandable_event_writer);
+            }
+            Err(err) => {
+                warn!("Failed to get query result: {:?}", err);
+                continue;
+            }
+        }
     }
 }
