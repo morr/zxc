99f827091d369256644dc29472f5f10eb83a3d85
Andrey Sidorov <takandar@gmail.com>
2024-05-22 22:56:01 +0300

    new logic in WorkOn command

--- src/commandable/work_on_command/mod.rs
+++ src/commandable/work_on_command/mod.rs
@@ -15,35 +15,48 @@ pub struct WorkOnCommand(pub Entity, pub Task);
 fn execute_command(
     mut commands: Commands,
     mut command_reader: EventReader<WorkOnCommand>,
-    mut query: Query<(&mut Pawn, &mut Commandable)>,
-    mut event_writer: EventWriter<WorkStartEvent>,
-    mut commandable_event_writer: EventWriter<CommandExecutedEvent>,
+    // mut pawn_query: Query<(&mut Pawn, &mut Commandable)>,
+    mut workable_query: Query<&mut Workable>,
+    // mut event_writer: EventWriter<WorkStartEvent>,
+    // mut commandable_event_writer: EventWriter<CommandExecutedEvent>,
     // mut pawn_state_change_event_writer: EventWriter<EntityStateChangeEvent<PawnState>>,
 ) {
-    for WorkOnCommand(entity, task) in command_reader.read() {
+    for WorkOnCommand(_commandable_entity, task) in command_reader.read() {
         // println!("{:?}", WorkOnCommand(*entity, task.clone()));
-        match query.get_mut(*entity) {
-            Ok((mut pawn, mut commandable)) => {
-                commandable.complete_executing(
-                    *entity,
-                    &mut commands,
-                    &mut commandable_event_writer,
-                );
 
-                pawn.change_state(
-                    PawnState::TaskAssigned(task.clone()),
-                    *entity,
-                    &mut commands,
-                    // &mut pawn_state_change_event_writer,
-                );
-                event_writer.send(WorkStartEvent {
-                    pawn_entity: *entity,
-                });
+        match workable_query.get_mut(task.entity) {
+            Ok(mut workable) => {
+                workable.change_state(WorkableState::BeingWorked, task.entity, &mut commands);
             }
+
             Err(err) => {
                 warn!("Failed to get query result: {:?}", err);
                 continue;
             }
         }
+
+        // match pawn_query.get_mut(*entity) {
+        //     Ok((mut pawn, mut commandable)) => {
+        //         commandable.complete_executing(
+        //             *entity,
+        //             &mut commands,
+        //             &mut commandable_event_writer,
+        //         );
+        //
+        //         pawn.change_state(
+        //             PawnState::TaskAssigned(task.clone()),
+        //             *entity,
+        //             &mut commands,
+        //             // &mut pawn_state_change_event_writer,
+        //         );
+        //         event_writer.send(WorkStartEvent {
+        //             pawn_entity: *entity,
+        //         });
+        //     }
+        //     Err(err) => {
+        //         warn!("Failed to get query result: {:?}", err);
+        //         continue;
+        //     }
+        // }
     }
 }
