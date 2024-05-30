614ccdd72526934f55024b34a59c3747008f38e4
Andrey Sidorov <takandar@gmail.com>
2024-05-30 22:00:48 +0300

    updates to Bed logic

--- src/structure/bed/mod.rs
+++ src/structure/bed/mod.rs
@@ -8,8 +8,18 @@ impl Plugin for BedPlugin {
     }
 }
 
-#[derive(Resource, Deref, DerefMut, Default)]
+#[derive(Resource, Debug, Deref, DerefMut, Default)]
 pub struct AvailableBeds(pub u32);
+
+impl AvailableBeds {
+    pub fn increment(&mut self) {
+        self.0 += 1;
+    }
+
+    pub fn decrement(&mut self) {
+        self.0 -= 1;
+    }
+}
 
 #[derive(Component, Reflect, Default)]
 pub struct Bed {
@@ -18,20 +28,20 @@ pub struct Bed {
 
 impl Bed {
     pub fn spawn(
-        commands: &mut Commands,
-        assets: &Res<TextureAssets>,
-        navmesh: &mut Navmesh,
         grid_tile: IVec2,
-        // state_change_event_writer: &mut EventWriter<EntityStateChangeEvent<FarmState>>,
+        commands: &mut Commands,
+        texture: Handle<Image>,
+        navmesh: &mut Navmesh,
+        available_beds: &mut ResMut<AvailableBeds>, 
     ) {
         let size = IVec2::new(BED_SIZE, BED_SIZE);
 
         let id = commands
             .spawn((
                 Bed::default(),
-                Name::new("bed"),
+                Name::new("Bed"),
                 SpriteBundle {
-                    texture: assets.bed.clone(),
+                    texture,
                     sprite: Sprite {
                         custom_size: Some(size.grid_tile_edge_to_world()),
                         ..default()
@@ -55,5 +65,7 @@ impl Bed {
             Navtile::config_cost_to_pathfinding_cost(CONFIG.movement_cost.furniture),
         );
         navmesh.add_occupation::<Bed>(id, grid_tile.x, grid_tile.y);
+
+        available_beds.increment();
     }
 }
