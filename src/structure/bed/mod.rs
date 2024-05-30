09799a4525e1d7657b38497505173d8927be706f
Andrey Sidorov <takandar@gmail.com>
2024-05-30 21:19:17 +0300

    Bed::spawn

--- src/structure/bed/mod.rs
+++ src/structure/bed/mod.rs
@@ -8,10 +8,52 @@ impl Plugin for BedPlugin {
     }
 }
 
+#[derive(Resource, Deref, DerefMut, Default)]
+pub struct AvailableBeds(pub u32);
+
 #[derive(Component, Reflect, Default)]
 pub struct Bed {
     pub owner: Option<Entity>,
 }
 
-#[derive(Resource, Deref, DerefMut, Default)]
-pub struct AvailableBeds(pub u32);
+impl Bed {
+    pub fn spawn(
+        commands: &mut Commands,
+        assets: &Res<TextureAssets>,
+        navmesh: &mut Navmesh,
+        grid_tile: IVec2,
+        // state_change_event_writer: &mut EventWriter<EntityStateChangeEvent<FarmState>>,
+    ) {
+        let size = IVec2::new(BED_SIZE, BED_SIZE);
+
+        let id = commands
+            .spawn((
+                Bed::default(),
+                Name::new("bed"),
+                SpriteBundle {
+                    texture: assets.bed.clone(),
+                    sprite: Sprite {
+                        custom_size: Some(size.grid_tile_edge_to_world()),
+                        ..default()
+                    },
+                    transform: Transform::from_translation(
+                        (grid_tile.grid_tile_edge_to_world()
+                            + size.grid_tile_edge_to_world() / 2.0)
+                            .extend(STRUCTURE_Z_INDEX),
+                    ),
+                    ..default()
+                },
+            ))
+            // .insert(ShowAabbGizmo {
+            //     colo: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
+            // })
+            .id();
+
+        navmesh.update_cost(
+            (grid_tile.x)..(grid_tile.x + size.x),
+            (grid_tile.y)..(grid_tile.y + size.y),
+            Navtile::config_cost_to_pathfinding_cost(CONFIG.movement_cost.furniture),
+        );
+        navmesh.add_occupation::<Bed>(id, grid_tile.x, grid_tile.y);
+    }
+}
--- src/structure/systems.rs
+++ src/structure/systems.rs
@@ -2,10 +2,10 @@ use super::*;
 
 pub const BASE_WIDTH: i32 = 8;
 pub const BASE_HEIGHT: i32 = 14;
-pub const FARM_TILE_SIZE: i32 = 1;
 pub const HOUSE_WIDTH: i32 = 4;
 pub const HOUSE_HEIGHT: i32 = 4;
 pub const WELL_SIZE: i32 = 2;
+pub const FARM_TILE_SIZE: i32 = 1;
 pub const BED_SIZE: i32 = 1;
 
 pub fn spawn_base(
@@ -145,40 +145,44 @@ pub fn spawn_bed(
     assets: Res<TextureAssets>,
     arc_navmesh: ResMut<ArcNavmesh>,
 ) {
-    let size = IVec2::new(BED_SIZE, BED_SIZE);
+    let mut navmesh = arc_navmesh.write();
 
     for x in 0..CONFIG.starting_scene.beds_num {
         let grid_tile = IVec2::new(-7 + x * BED_SIZE, 3);
 
-        let id = commands
-            .spawn((
-                Bed::default(),
-                Name::new("bed"),
-                SpriteBundle {
-                    texture: assets.bed.clone(),
-                    sprite: Sprite {
-                        custom_size: Some(size.grid_tile_edge_to_world()),
-                        ..default()
-                    },
-                    transform: Transform::from_translation(
-                        (grid_tile.grid_tile_edge_to_world()
-                            + size.grid_tile_edge_to_world() / 2.0)
-                            .extend(STRUCTURE_Z_INDEX),
-                    ),
-                    ..default()
-                },
-            ))
-            // .insert(ShowAabbGizmo {
-            //     color: Some(Color::rgba(1.0, 1.0, 1.0, 0.25)),
-            // })
-            .id();
-
-        let mut navmesh = arc_navmesh.write();
-        navmesh.update_cost(
-            (grid_tile.x)..(grid_tile.x + size.x),
-            (grid_tile.y)..(grid_tile.y + size.y),
-            Navtile::config_cost_to_pathfinding_cost(CONFIG.movement_cost.furniture),
+        Bed::spawn(
+            &mut commands,
+            &assets,
+            &mut navmesh,
+            grid_tile,
         );
-        navmesh.add_occupation::<Bed>(id, grid_tile.x, grid_tile.y);
     }
 }
+
+// pub fn spawn_farm(
+//     mut commands: Commands,
+//     assets: Res<FarmAssets>,
+//     arc_navmesh: ResMut<ArcNavmesh>,
+//     mut state_change_event_writer: EventWriter<EntityStateChangeEvent<FarmState>>,
+// ) {
+//     let grid_tile_start = IVec2::new(-13, 0);
+//     let mut navmesh = arc_navmesh.write();
+//
+//     for x in 0..CONFIG.starting_scene.farm_width {
+//         for y in 0..CONFIG.starting_scene.farm_height {
+//             let grid_tile = IVec2::new(
+//                 grid_tile_start.x + FARM_TILE_SIZE * x,
+//                 grid_tile_start.y + FARM_TILE_SIZE * y,
+//             );
+//
+//             Farm::spawn(
+//                 &mut commands,
+//                 &assets,
+//                 &mut navmesh,
+//                 grid_tile,
+//                 &mut state_change_event_writer,
+//            );
+//         }
+//     }
+// }
+ 
