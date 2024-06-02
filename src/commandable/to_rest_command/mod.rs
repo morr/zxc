fbc716872a3aaca4e604b9b5fe48e92a5d1a277d
Andrey Sidorov <takandar@gmail.com>
2024-06-03 01:50:45 +0300

    pawn now finds empty tile to sleepn on

--- src/commandable/to_rest_command/mod.rs
+++ src/commandable/to_rest_command/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 use super::*;
 
 pub struct ToRestCommandPlugin;
@@ -20,56 +18,66 @@ pub struct ToRestCommand {
 fn execute_command(
     mut commands: Commands,
     mut bed_query: Query<(Entity, &Transform, &mut Bed)>,
-    mut commandable_query: Query<(&mut Pawn, &mut Commandable)>,
+    mut commandable_query: Query<(&mut Pawn, &mut Commandable, &Transform)>,
     mut command_reader: EventReader<ToRestCommand>,
     mut commandable_event_writer: EventWriter<CommandCompleteEvent>,
     mut available_beds: ResMut<AvailableBeds>,
     mut commandable_interrupt_writer: EventWriter<InternalCommandInterruptEvent>,
     mut tasks_scheduler: EventWriter<ScheduleTaskEvent>,
+    arc_navmesh: Res<ArcNavmesh>,
 ) {
     for ToRestCommand { commandable_entity } in command_reader.read() {
         match commandable_query.get_mut(*commandable_entity) {
-            Ok((mut pawn, mut commandable)) => {
+            Ok((mut pawn, mut commandable, pawn_transform)) => {
                 commandable.complete_executing(
                     *commandable_entity,
                     &mut commands,
                     &mut commandable_event_writer,
                 );
-                let sleep_command = CommandType::Sleep(SleepCommand {
-                    commandable_entity: *commandable_entity,
-                });
-                let mut commands_queue = VecDeque::from([sleep_command]);
 
-                if let Some(bed_entity) = pawn.owned_bed {
+                let grid_tile = if let Some(bed_entity) = pawn.owned_bed {
                     // move to claimed bed
-                    let (_entity, transform, _bed) = bed_query.get(bed_entity).unwrap();
+                    let (_entity, bed_transform, _bed) = bed_query.get(bed_entity).unwrap();
 
-                    commands_queue.push_front(CommandType::MoveTo(MoveToCommand {
-                        commandable_entity: *commandable_entity,
-                        grid_tile: transform.translation.truncate().world_pos_to_grid(),
-                    }))
+                    bed_transform.translation.truncate().world_pos_to_grid()
                 } else if available_beds.0 > 0 {
                     // or claim fee bed
-                    for (bed_entity, transform, mut bed) in bed_query.iter_mut() {
-                        if bed.owner.is_none() {
-                            bed.claim_by(
-                                bed_entity,
-                                *commandable_entity,
-                                &mut pawn,
-                                &mut available_beds,
-                            );
-                            commands_queue.push_front(CommandType::MoveTo(MoveToCommand {
-                                commandable_entity: *commandable_entity,
-                                grid_tile: transform.translation.truncate().world_pos_to_grid(),
-                            }));
-
-                            break;
+                    let mut found_bed_tile = None;
+                    for (bed_entity, bed_transform, mut bed) in bed_query.iter_mut() {
+                        if bed.owner.is_some() {
+                            continue;
                         }
+                        bed.claim_by(
+                            bed_entity,
+                            *commandable_entity,
+                            &mut pawn,
+                            &mut available_beds,
+                        );
+                        found_bed_tile =
+                            Some(bed_transform.translation.truncate().world_pos_to_grid());
+                        break;
                     }
-                }
+                    found_bed_tile.unwrap()
+                } else {
+                    // go to random nearest empty place
+                    find_valid_end_tile(
+                        pawn_transform.translation.truncate(),
+                        &arc_navmesh.read(),
+                        &mut rand::thread_rng(),
+                        0,
+                    )
+                };
 
                 commandable.set_queue(
-                    commands_queue,
+                    [
+                        CommandType::MoveTo(MoveToCommand {
+                            commandable_entity: *commandable_entity,
+                            grid_tile,
+                        }),
+                        CommandType::Sleep(SleepCommand {
+                            commandable_entity: *commandable_entity,
+                        }),
+                    ],
                     *commandable_entity,
                     &mut commands,
                     &mut commandable_interrupt_writer,
