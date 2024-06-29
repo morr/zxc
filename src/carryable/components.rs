69cf52afb93e6b3d638b4b5cc5a6712de2868062
Andrey Sidorov <takandar@gmail.com>
2024-06-29 18:59:52 +0300

    misc clippy fix

--- src/carryable/components.rs
+++ src/carryable/components.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-use bevy::{reflect::List, sprite::MaterialMesh2dBundle};
+use bevy::sprite::MaterialMesh2dBundle;
 
 #[derive(Debug, Clone, Copy, Reflect, PartialEq, Eq)]
 pub enum CarryableKind {
