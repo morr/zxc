9a9de8f5cea8f579b41e7d1a75fb5b787c9fee66
Andrey Sidorov <takandar@gmail.com>
2024-06-30 21:41:00 +0300

    Revert "extend_queue for feeding command instead interrupting queue and setting"
    This reverts commit 7e2af4b48b58f54cae69225c368bf5538bcecda5.

--- src/feedable/mod.rs
+++ src/feedable/mod.rs
@@ -50,7 +50,9 @@ fn progress_hunger(
     time: Res<Time>,
     time_scale: Res<TimeScale>,
     mut query: Query<(Entity, &mut Feedable, &mut Commandable)>,
-    food_stock: Res<FoodStock>,
+    mut commandable_interrupt_writer: EventWriter<InternalCommandInterruptEvent>,
+    mut commandable_release_resources_writer: EventWriter<ReleaseCommandResourcesEvent>,
+    food_stock: Res<FoodStock>
 ) {
     let time_amount = time_scale.scale_to_seconds(time.delta_seconds());
 
@@ -60,10 +62,12 @@ fn progress_hunger(
         feedable.progress_hunger(time_amount);
 
         if wasnt_overflowed && feedable.is_overflowed() && food_stock.amount > 0 {
-            commandable.extend_queue(
+            commandable.set_queue(
                 CommandType::Feed(FeedCommand { commandable_entity }),
                 commandable_entity,
                 &mut commands,
+                &mut commandable_interrupt_writer,
+                &mut commandable_release_resources_writer,
             );
         }
     }
