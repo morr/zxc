9435b2d8392ee3714d063a2fe99a754d7efd3d3f
Andrey Sidorov <takandar@gmail.com>
2024-07-12 22:38:33 +0300

    reduce nesting level

--- src/ui/hovered/mod.rs
+++ src/ui/hovered/mod.rs
@@ -64,94 +64,92 @@ fn update_ui_on_hover_event(
         }
     }
 
-    if let Some(grid_tile) = possibly_hovered_grid_tiles.last() {
-        let hovered_root_ui_id = hovered_root_ui_query.get_single().unwrap();
-        let mut hovered_root_ui_commands = commands.entity(hovered_root_ui_id);
-        hovered_root_ui_commands.despawn_descendants();
+    let Some(grid_tile) = possibly_hovered_grid_tiles.last() else { return };
 
-        let navmesh = arc_navmesh.read();
+    let hovered_root_ui_id = hovered_root_ui_query.get_single().unwrap();
+    let mut hovered_root_ui_commands = commands.entity(hovered_root_ui_id);
+    hovered_root_ui_commands.despawn_descendants();
 
-        // hover over Tile
-        for tile_id in navmesh.get_occupants::<Tile>(grid_tile.x, grid_tile.y) {
-            render_tile_ui(
-                *tile_id,
+    let navmesh = arc_navmesh.read();
+
+    // hover over Tile
+    for tile_id in navmesh.get_occupants::<Tile>(grid_tile.x, grid_tile.y) {
+        render_tile_ui(
+            *tile_id,
+            &mut hovered_root_ui_commands,
+            *grid_tile,
+            &font_assets,
+            UiOpacity::Medium,
+        );
+    }
+
+    // hover over Bed
+    for bed_id in navmesh.get_occupants::<Bed>(grid_tile.x, grid_tile.y) {
+        if let Ok(bed) = bed_query.get(*bed_id) {
+            render_bed_ui(
+                *bed_id,
                 &mut hovered_root_ui_commands,
-                *grid_tile,
+                bed,
                 &font_assets,
                 UiOpacity::Medium,
             );
         }
+    }
 
-        // hover over Bed
-        for bed_id in navmesh.get_occupants::<Bed>(grid_tile.x, grid_tile.y) {
-            if let Ok(bed) = bed_query.get(*bed_id) {
-                render_bed_ui(
-                    *bed_id,
-                    &mut hovered_root_ui_commands,
-                    bed,
-                    &font_assets,
-                    UiOpacity::Medium,
-                );
-            }
+    // hover over Storage
+    for storage_id in navmesh.get_occupants::<Storage>(grid_tile.x, grid_tile.y) {
+        if let Ok(storage) = storage_query.get(*storage_id) {
+            render_storage_ui(
+                *storage_id,
+                &mut hovered_root_ui_commands,
+                storage,
+                &font_assets,
+                UiOpacity::Medium,
+            );
         }
+    }
 
-        // hover over Storage
-        for storage_id in navmesh.get_occupants::<Storage>(grid_tile.x, grid_tile.y) {
-            if let Ok(storage) = storage_query.get(*storage_id) {
-                render_storage_ui(
-                    *storage_id,
-                    &mut hovered_root_ui_commands,
-                    storage,
-                    &font_assets,
-                    UiOpacity::Medium,
-                );
-            }
+    // hover over Pawn
+    for pawn_id in navmesh.get_occupants::<Pawn>(grid_tile.x, grid_tile.y) {
+        if let Ok((pawn, movable, restable, feedable, commandable)) = pawn_query.get(*pawn_id) {
+            render_pawn_ui(
+                *pawn_id,
+                &mut hovered_root_ui_commands,
+                pawn,
+                movable,
+                restable,
+                feedable,
+                commandable,
+                &font_assets,
+                UiOpacity::Medium,
+            );
         }
+    }
 
-        // hover over Pawn
-        for pawn_id in navmesh.get_occupants::<Pawn>(grid_tile.x, grid_tile.y) {
-            if let Ok((pawn, movable, restable, feedable, commandable)) =
-                pawn_query.get(*pawn_id)
-            {
-                render_pawn_ui(
-                    *pawn_id,
-                    &mut hovered_root_ui_commands,
-                    pawn,
-                    movable,
-                    restable,
-                    feedable,
-                    commandable,
-                    &font_assets,
-                    UiOpacity::Medium,
-                );
-            }
+    // hover over Farm
+    for farm_id in navmesh.get_occupants::<Farm>(grid_tile.x, grid_tile.y) {
+        if let Ok((farm, workable)) = farm_query.get(*farm_id) {
+            render_farm_ui(
+                *farm_id,
+                &mut hovered_root_ui_commands,
+                farm,
+                workable,
+                &font_assets,
+                UiOpacity::Medium,
+            );
         }
+    }
 
-        // hover over Farm
-        for farm_id in navmesh.get_occupants::<Farm>(grid_tile.x, grid_tile.y) {
-            if let Ok((farm, workable)) = farm_query.get(*farm_id) {
-                render_farm_ui(
-                    *farm_id,
-                    &mut hovered_root_ui_commands,
-                    farm,
-                    workable,
-                    &font_assets,
-                    UiOpacity::Medium,
-                );
-            }
-        }
-
-        // hover over Carryable
-        for carryable_id in navmesh.get_occupants::<Carryable>(grid_tile.x, grid_tile.y) {
-            if let Ok(carryable) = carryable_query.get(*carryable_id) {
-                render_carryable_ui(
-                    *carryable_id,
-                    &mut hovered_root_ui_commands,
-                    carryable,
-                    &font_assets,
-                    UiOpacity::Medium,
-                );
-            }
+    // hover over Carryable
+    for carryable_id in navmesh.get_occupants::<Carryable>(grid_tile.x, grid_tile.y) {
+        if let Ok(carryable) = carryable_query.get(*carryable_id) {
+            render_carryable_ui(
+                *carryable_id,
+                &mut hovered_root_ui_commands,
+                carryable,
+                &font_assets,
+                UiOpacity::Medium,
+            );
         }
     }
 }
