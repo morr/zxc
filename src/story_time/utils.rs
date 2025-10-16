37ac317d573530634889575049257af83ac0396f
Andrey Sidorov <takandar@gmail.com>
2025-10-16 21:13:13 +0300

    story time utils update

--- src/story_time/utils.rs
+++ src/story_time/utils.rs
@@ -8,12 +8,12 @@ pub fn days_to_seconds(days: f32) -> f32 {
     days * config().time.day_duration
 }
 
-pub fn starting_seconds() -> f32 {
-    config().time.hour_duration * config().starting_scene.day_hour as f32
+pub fn total_seconds(elapsed_seconds: f32) -> f32 {
+    elapsed_seconds + config().time.hour_duration * config().starting_scene.day_hour as f32
 }
 
 pub fn total_days(elapsed_seconds: f32) -> u32 {
-    ((starting_seconds() + elapsed_seconds) / config().time.day_duration).floor() as u32
+    (total_seconds(elapsed_seconds) / config().time.day_duration).floor() as u32
 }
 
 pub fn total_day_to_year_day(total_day: u32) -> u32 {
@@ -49,46 +49,30 @@ pub fn year_day_to_season_day_label(year_day: u32) -> String {
         year_day_to_season(year_day),
     )
 }
-//
-//     pub fn total_seconds(&self) -> f32 {
-//         self.0.floor()
-//     }
-//
-//     pub fn day_time(&self) -> f32 {
-//         (self.0 % config().time.day_duration) / config().time.day_duration
-//     }
-//
-//     pub fn total_days(&self) -> u32 {
-//         (self.0 / config().time.day_duration).floor() as u32
-//     }
-//
-//     pub fn year_day(&self) -> u32 {
-//         Self::total_day_to_year_day(self.total_days())
-//     }
-//
-//     // pub fn season_index(&self) -> u32 {
-//     //     (self.total_days() / config().time.days_in_season) % config().time.seasons_in_year
-//     // }
-//
-//     pub fn season_day(&self) -> u32 {
-//         Self::year_day_to_season_day(self.year_day())
-//     }
-//
-//     pub fn year(&self) -> u32 {
-//         1 + self.total_days() / config().time.days_in_year
-//     }
-//
-//     pub fn year_season(&self) -> YearSeason {
-//         Self::year_day_to_season(self.year_day())
-//     }
-//
-//     pub fn day_hour(&self) -> u32 {
-//         ((self.0 % config().time.day_duration) / config().time.hour_duration).floor() as u32
-//     }
-//
-//     pub fn hour_minute(&self) -> u32 {
-//         (((self.0 % config().time.day_duration) % config().time.hour_duration)
-//             / config().time.minute_duration)
-//             .floor() as u32
-//     }
+
+pub fn year_day(elapsed_seconds: f32) -> u32 {
+    total_day_to_year_day(total_days(elapsed_seconds))
+}
+
+pub fn current_season_day(elapsed_seconds: f32) -> u32 {
+    year_day_to_season_day(year_day(elapsed_seconds))
+}
+
+pub fn current_year(elapsed_seconds: f32) -> u32 {
+    1 + total_days(elapsed_seconds) / config().time.days_in_year
+}
+
+pub fn current_year_season(elapsed_seconds: f32) -> YearSeason {
+    year_day_to_season(year_day(elapsed_seconds))
+}
+
+pub fn current_day_hour(elapsed_seconds: f32) -> u32 {
+    ((total_seconds(elapsed_seconds) % config().time.day_duration) / config().time.hour_duration).floor() as u32
+}
+
+pub fn current_hour_minute(elapsed_seconds: f32) -> u32 {
+    (((total_seconds(elapsed_seconds) % config().time.day_duration) % config().time.hour_duration)
+        / config().time.minute_duration)
+        .floor() as u32
+}
 // }
